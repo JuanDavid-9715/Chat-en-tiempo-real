934c3101fcd3ded9acd99e9c2488d5465dfde2de
Juan David Medellin Calderon <medellincalderonjuandavid@gmail.com>
2023-01-18 19:03:36 -0500

    Conexión a la base de datos

---
+++ server/config/mongo.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+    const DB_URI = process.env.DB_URI;
+
+    mongoose.set("strictQuery", true);
+
+    mongoose.connect(DB_URI, (err) => {
+        if (!err) {
+            console.log("********** CONEXIÓN CORRECTA **********");
+        } else {
+            console.log("********** ERROR DE CONEXIÓN **********");
+            console.log(err);
+        }
+    });
+};
+
+module.exports = { dbConnect };
